315049ec57dcf974d475ef7a196f5c3c52414fa7
Catherine Kim <catherine@ipwe.com>
2020-08-08 10:35:18 -0700

    Add updated searching and test_searching files Because: - Updated code so that the test cases work in the test file.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -3,52 +3,34 @@
 # Initially, the whole array is our search space
 # at each step, we find the mid value in the search space and compare it with target value
 # Find out if a key target exists in the sorted list 
-def binary_search(arr, start, end, target):  
+def binary_search(arr, target, start, end):  
     # start = 0
-    # end = n-1
+    # end = n - 1
     # Base condition (search space is exhausted)
     if start > end:
-        return -1
-        
+        return - 1
+    
     # we find the mid space in the search space and compares it with key value
     mid = (start + end) // 2
     
-    # overflow can happen. Use below 
-    # mid = left + (right - left) / 2
-        
-    # Compare target with the middle element - if element is present at the middle itself
+    
+    # Compare target with the middle element 
     # Base condition (key value is found)
     if target == arr[mid]:
         # If target matches with the middle element, return the mid index
         return mid
     
-    # if target value is smaller than the middle element in the array,
+            # if target value is smaller than the middle element in the array,
     elif target < arr[mid]:
         # discard all elements in the right search space including the mid element i.e. arr[mid...high]
         return binary_search(arr, start, mid - 1, target)
         # now our new hgh would be mid - 1
         
-    else:
-        # otherwise, discard all elements in the left search space including the mid element i.e. arr[low...mid]
+    else: # if target value is greater than the middle element in the array,
+        # discard all elements in the left search space including the mid element i.e. arr[low...mid]
         return binary_search(arr, mid + 1, end, target)
         # now our new low would be mid + 1
-# Repeat the process until target is found or the search space is exhausted
-         
-# Test
-if __name__ == '__main__':
-    
-    arr = [2, 5, 6, 8, 9, 10]
-    key = 11
-
-    (start, end) = (0, len(arr) - 1)
-    # Function call
-    index = binary_search(arr, start, end, key)
-
-    if index != -1:
-        print("Element is present at index ", index)
-    else:
-        print("Element is not present in the array")        
-
+    # Repeat the process until target is found or the search space is exhausted
 
 
 
